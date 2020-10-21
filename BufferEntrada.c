fb2a93e8b17777c9503de1016e12e66be0b28ae0
Nuisigor <dexchanel@gmail.com>
2020-10-21 15:45:58 -0300

    Funções da Entrada

--- BufferEntrada.c
+++ BufferEntrada.c
@@ -3,24 +3,42 @@
 #include <stdio.h>
 #include "BufferEntrada.h"
 
-ITEM_VENDA* iv_Criar_E(char* arquivo_entrada ,unsigned int N_registros, FILE** retorno){
+static ITEM_VENDA* iv_Ler_Novo(unsigned int N_registros, FILE** retorno){
     ITEM_VENDA* iv = malloc(sizeof(ITEM_VENDA)*N_registros);
-    *retorno = fopen(arquivo_entrada, "r");
-
-    fread(iv,sizeof(ITEM_VENDA), N_registros, *retorno);
-
+    fread(iv, sizeof(ITEM_VENDA), N_registros, *retorno);
     return iv;
 }
 
+BUFF* iv_Criar_E(char* arquivo_entrada ,unsigned int N_registros, FILE** retorno){
+    ITEM_VENDA* iv = malloc(sizeof(ITEM_VENDA)*N_registros);
+    BUFF* b = malloc(sizeof(BUFF));
 
-/*
-    Pra busca do próximo dá pra fazer uma struct com a posição atual ou
-    fazer uma váriavel a mais em cada função(Próximo, Consumir e Vazio)
-*/
-ITEM_VENDA iv_Proximo(ITEM_VENDA* buffer){}
-ITEM_VENDA iv_Consumir(ITEM_VENDA* buffer){}
-int iv_Vazio(char* arquivo_entrada);
+    if(*retorno == NULL)
+        *retorno = fopen(arquivo_entrada, "r");
 
+    b->iv = iv_Ler_Novo(N_registros, retorno);
+    b->pos = 0;
+    b->tam = N_registros;
+    b->arq = retorno;
+    return b;
+}
+
+ITEM_VENDA iv_Proximo(BUFF* buffer){
+    return buffer->iv[buffer->pos];
+}
+
+ITEM_VENDA iv_Consumir(BUFF* buffer){
+    if(buffer->pos == buffer->tam){
+        free(buffer->iv);
+        buffer->iv = iv_Ler_Novo(buffer->tam, buffer->arq);
+    }
+
+    return buffer->iv[buffer->pos++];
+}
+
+int iv_Vazio(FILE** arq){
+    return feof(*arq);
+}
 
 void iv_Destruir(FILE** retorno, ITEM_VENDA* registros){
     fclose(*retorno);
