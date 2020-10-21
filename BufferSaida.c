4470a753fc6e4e355adb3a902ff4842e958dd709
getuliobr <getulioregis@alunos.utfpr.edu.br>
2020-10-21 16:34:45 -0300

    arrumei identacao

--- BufferSaida.c
+++ BufferSaida.c
@@ -27,8 +27,8 @@ void iv_Inserir(BUFF* buffer, ITEM_VENDA registro){
 void iv_Despejar(BUFF* buffer){
     fwrite(buffer->iv, sizeof(ITEM_VENDA), buffer->tam, *buffer->arq);
     free(buffer->iv);
-        buffer->iv = iv_Criar(buffer->tam);
-        buffer->tam = 0;
+    buffer->iv = iv_Criar(buffer->tam);
+    buffer->tam = 0;
 }
 
 void iv_Destruir_S(BUFF* buffer){
