b1d9d14de8a1bda29e52bb338dd7470b4262303a
Nuisigor <dexchanel@gmail.com>
2020-10-24 16:15:44 -0300

    sabado

--- main.c
+++ main.c
@@ -17,7 +17,8 @@ int main(){
    
    */
     FILE* arq_principal = NULL;
-    char** arq_ordenados = malloc(sizeof(char*)*1);
+    char** arq_ordenados = malloc(sizeof(char*)*1); // Guarda nome dos arquivos já ordenados
+    int arq_ordenados_count;                              // Guarda a quantidade de arquivos ordenados
     while(1){
         BUFF* entrada = iv_Criar_E("teste.dat", 10, &arq_principal);
         if(feof(arq_principal)) break;
@@ -26,12 +27,13 @@ int main(){
         strcpy(arqsaida, "arqsaida");            // ^
         char x = i+'0';                         // ^
         strncat(arqsaida, &x, 1);                   
-        strcat(arqsaida, ".txt");              // ^
+        strcat(arqsaida, ".dat");              // ^
 
         FILE* novo = NULL;                             // Referencia novo ponteiro de file
         BUFF* saida = iv_Criar_S(arqsaida, 10, &novo); // Cria um BUFFER de saida com o ponteiro e nome criados
         arq_ordenados[i] =  arqsaida;
         i++;
+        arq_ordenados_count = i;
         realloc(arq_ordenados, sizeof(char*)*i);
         free(saida->iv);            // Copia o Buffer de entrada já ordenado, o tamanho e posição
         saida->iv = entrada->iv;    // ^
@@ -45,4 +47,25 @@ int main(){
     Criar um novo arquivo e vai colocando de N em N MB fazendo o mergeSort
     Obs: O Buffer vai se atualizando sozinho
     */
+    FILE* arq_saida_principal = NULL;
+    BUFF* arq_saida_p = iv_Criar_S("teste.txt", 10, &arq_saida_principal);
+
+    FILE** arq_entrada_ordenados = malloc(sizeof(FILE*)*arq_ordenados_count);
+    BUFF* arq_entrada_ord = malloc(sizeof(BUFF*)*arq_ordenados_count);
+    while(1){
+        for( i = 0; i < arq_ordenados_count; i++){
+            arq_entrada_ordenados[i] = NULL;
+            arq_entrada_ord = iv_Criar_E(arq_ordenados[i], 10, &arq_entrada_ordenados[i]);
+        }
+        int x;
+        for( i = 0; i < arq_ordenados_count; i++){
+            if(i == 0){
+                x = feof(arq_entrada_ordenados[i]);
+                continue;
+            }
+            x = x && feof(arq_entrada_ordenados[i]);
+        }
+        if(x)
+            break;
+    }
 }
